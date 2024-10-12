0005bafcbf1f3fad4a41a1ce81d1ecc726e4df0d
Bharadwajreddy1406 <bharadwajreddy146@gmail.com>
2024-10-13 01:20:25 +0530

    added local storage usecases

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,38 +1,45 @@
-import './App.css'
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
-import Home from './components/Home'
-import ReviewChat from './components/ReviewChat'
-import LoginSignUp from './components/LoginSignUp'
-import { AuthProvider, useAuth } from './Context/AuthContext'
-import Header from './components/shared/Header'
-import Profile from './components/Profile'
-import About from './components/About'
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Home from "./components/Home";
+import ReviewChat from "./components/ReviewChat";
+import LoginSignUp from "./components/LoginSignUp";
+import { AuthProvider, useAuth } from "./Context/AuthContext";
+import Header from "./components/shared/Header";
+import Profile from "./components/Profile";
+import About from "./components/About";
+import Loader from "./components/shared/Loader";
 
 function App() {
-
   const auth = useAuth();
-  
-  return (
 
+  if (auth.isLoading) {
+    return (
+      <Loader />
+    );
+  }
+
+  return (
     <Router>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        {auth.isLoggedIn &&
-        <>
-          <Route path="/review-chat" element={<ReviewChat />} />
-          <Route path="/profile" element={<Profile />} />
-        </>
-        }
+        {auth.isLoggedIn && (
+          <>
+            <Route path="/review-chat" element={<ReviewChat />} />
+            <Route path="/profile" element={<Profile />} />
+          </>
+        )}
         <Route path="/login" element={<LoginSignUp />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
-
-
-
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/Context/AuthContext.jsx
+++ frontend/src/Context/AuthContext.jsx
@@ -1,24 +1,27 @@
 import { createContext, useState, useContext, useEffect } from "react";
 import { LoginUser, LogoutUser, checkAuthStatus } from "../Helpers/apiComms";
-import { User, AuthContextType } from "./User";
 
-const AuthContext = createContext(new AuthContextType|null);
+// Create the AuthContext with null as the initial value
+const AuthContext = createContext(null);
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     async function checkStatus() {
       try {
         const data = await checkAuthStatus();
         if (data) {
-          const user = new User(data.username, data.email);
+          const user = { username: data.username, email: data.email };
           setUser(user);
           setIsLoggedIn(true);
         }
       } catch (error) {
         console.error("Error checking auth status:", error);
+      } finally {
+        setIsLoading(false);
       }
     }
     checkStatus();
@@ -31,7 +34,7 @@ export const AuthProvider = ({ children }) => {
       if (!data) {
         throw new Error("Invalid login");
       }
-      const user = new User(data.username, data.email);
+      const user = { username: data.username, email: data.email };
       setUser(user);
       setIsLoggedIn(true);
     } catch (error) {
@@ -42,9 +45,7 @@ export const AuthProvider = ({ children }) => {
 
   const logout = async () => {
     try {
-      console.log("trying logout");
       await LogoutUser();
-      console.log("lemme logout");
       setUser(null);
       setIsLoggedIn(false);
       window.location.reload();
@@ -54,14 +55,14 @@ export const AuthProvider = ({ children }) => {
     }
   };
 
-  const value = { user, isLoggedIn, login, logout };
+  const value = { user, isLoggedIn, isLoading, login, logout };
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
 
 export const useAuth = () => {
   const context = useContext(AuthContext);
-  if (context === undefined) {
+  if (context === null) {
     throw new Error("useAuth must be used within an AuthProvider");
   }
   return context;
