d92c2aec6eed262d10b089cd0e246f3f29b80983
Bharadwajreddy1406 <bharadwajreddy146@gmail.com>
2024-10-13 16:36:57 +0530

    minor route issues

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,25 +3,35 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import Home from './components/Home';
 import ReviewChat from './components/ReviewChat';
 import LoginSignUp from './components/LoginSignUp';
-import { AuthProvider } from './Context/AuthContext'; // Removed useAuth here
+import { useAuth } from './Context/AuthContext';
 import Header from './components/shared/Header';
 import Profile from './components/Profile';
 import About from './components/About';
 
 function App() {
+  const auth = useAuth();
+
   return (
-  
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/review-chat" element={<ReviewChat />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/login" element={<LoginSignUp />} />
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-      </Router>
+    <Router>
+      {auth.isLoggedIn && <Header />}
+      <Routes>
+        {auth.isLoggedIn ? (
+          <>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/review-chat" element={<ReviewChat />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/login" element={<Navigate to="/" />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </>
+        ) : (
+          <>
+            <Route path="/login" element={<LoginSignUp />} />
+            <Route path="*" element={<Navigate to="/login" />} />
+          </>
+        )}
+      </Routes>
+    </Router>
   );
 }
 
