eb7b038e0834f69bc9429a74b9a5ba48a9edd00c
abhinav <abhinav151219@gmail.com>
2024-11-14 15:43:03 +0530

    changed the stucture of product details scrape and storage

--- fastapi/app/Helpers/embeddingAndFormat.py
+++ fastapi/app/Helpers/embeddingAndFormat.py
@@ -1,17 +1,13 @@
 
 from app.Model.embeddingModel import tokenize
 
-
-def transform_data(data, ASIN_NO):
+def add_review(curr_data, review_data):
     try:
-        if not isinstance(data, dict):
+        if not isinstance(curr_data, dict):
             raise ValueError("Input data is not a dictionary")
-        formated_data = {
-            "product_asin_no": ASIN_NO,
-            "average_rating": data["rating"],
-            "reviews": []
-        }
-        for review in data["customer_reviews"]:
+        if not isinstance(review_data, dict):
+            raise ValueError("New data is not a dictionary")
+        for review in review_data["customer_reviews"]:
             
             date_str = review["date"]
         
@@ -19,12 +15,35 @@ def transform_data(data, ASIN_NO):
             review_embeddings = tokenize(review["review"])
             title_embeddings = tokenize(review["title"])            
 
-            formated_data["reviews"].append({
+            curr_data["reviews"].append({
                 "title": title_embeddings,
                 "review": review_embeddings,  
                 "star": review["rating"],
                 "date": review_date
             })
+        return curr_data
+        
+    except Exception as e:
+        print(f"Exception occurred while transforming data: {e}")
+        return {"success": "false", "error": str(e)}
+        
+    
+
+def transform_data(data, ASIN_NO):
+    try:
+        if not isinstance(data, dict):
+            raise ValueError("Input data is not a dictionary")
+        formated_data = {
+            "product_asin_no": ASIN_NO,
+            "tile": data["title"],
+            "brand":data["brand"],
+            "price": data["price"],
+            "image_url": data["images"],
+            "features": data["feature_bullets"],
+            "average_rating": data["average_rating"],
+            "reviews": []
+        }
+
     
         return formated_data
     except Exception as e:
--- fastapi/app/Helpers/scrapeAndStore.py
+++ fastapi/app/Helpers/scrapeAndStore.py
@@ -1,7 +1,7 @@
 import requests
 import os
 from dotenv import load_dotenv
-from app.Helpers.embeddingAndFormat import transform_data
+from app.Helpers.embeddingAndFormat import transform_data, add_review
 from app.DB.session import dbconnection
 
 
@@ -10,8 +10,6 @@ api_key = os.getenv('SCRAPER_API_KEY')
 if not api_key:
     raise Exception("SCRAPER_API_KEY environment variable not set")
 
-
-# Connect to MongoDB
 db = dbconnection()
 products_collection = db['products']
 
@@ -21,38 +19,47 @@ def scrape_data(asin_no, domain="in"):
         "api_key": api_key,
         "asin": asin_no,
         "domain": domain,
-        "page": "1"
     }
-    url = "https://api.scrapingdog.com/amazon/reviews"
+    product_url = "https://api.scrapingdog.com/amazon/product"
+    review_url = "https://api.scrapingdog.com/amazon/reviews"
     try:
-        print("Sending request to scraper")
-        r = requests.get(url, params=payload)
-        print("Received response from ScraperAPI")
-        print(f"scraper Response status code: {r.status_code}")
-        data = r.json()
+        r = requests.get(product_url, params=payload)
+        print("Received product response from ScraperAPI")
         if r.status_code == 200:
+            product_data = r.json()
+            formatted_data = transform_data(product_data, asin_no)
+        for i in range(5):
+            payload["page"] = str(i)
+            print("Sending request to scraper")
+            r = requests.get(review_url, params=payload)
+            print("Received response from ScraperAPI, Response status code: {r.status_code}")
             
-            formatted_data = transform_data(data, asin_no)
-            print("Successfully transformed the data")
-            if isinstance(formatted_data, dict):
+            if r.status_code == 200:
                 
-                
-                products_collection.update_one(
-                    {"product_asin_no": asin_no},
-                    {"$set": formatted_data},
-                    upsert=True
-                )
-                print("Data successfully saved to MongoDB")
-                return formatted_data
+                review_data = r.json()
+                if(review_data["customer_reviews"] == []):
+                    break
+                formatted_data = add_review(formatted_data, review_data)
+                print("Successfully transformed the data")
             else:
-                return {"success": "false", "error": "Unexpected formatted data format"}
+                print("response status code of review scrape is not 200")
+                if(formatted_data["reviews"] == []):
+                    print("failed to get any reviews")
+                    
+                break
+                
+        if isinstance(formatted_data, dict):
+            print("updating data to MongoDB")
+            
+            products_collection.update_one(
+                {"product_asin_no": asin_no},
+                {"$set": formatted_data},
+                upsert=True
+            )
+            print("Data successfully saved to MongoDB")
+            return formatted_data
         else:
-            print(f"Error: {r.status_code}, Response: {data}")
-            if isinstance(data, dict):
-                return {"success": "false", "error": data}
-            else:
-                return {"success": "false", "error": "Unexpected response format"}
+            return {"success": "false", "error": "Unexpected formatted data format"}
     except Exception as e: 
         print("Exception occurred while scraping data" + str(e))
         return {"success": "false", "error": str(e)}
-scrape_data("B0D1VLS5KJ")
