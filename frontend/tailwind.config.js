367c1fe2b063125a1259e9d54d4e94581c3d70a9
WaryFriend456 <kushalbang456@gmail.com>
2024-11-20 00:17:05 +0530

    Removed Material Tailwind and completely switched to Aceternity UI and updated Login and Signup Page. Only Profile page is left

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,36 +1,44 @@
-const withMT = require("@material-tailwind/react/utils/withMT");
 const {
   default: flattenColorPalette,
 } = require("tailwindcss/lib/util/flattenColorPalette");
+const svgToDataUri = require("mini-svg-data-uri");
 
-module.exports = withMT({
-  content: ["./index.html", 
-    "./src/**/*.{vue,js,ts,jsx,tsx,mdx}",
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}"
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   darkMode: "class",
   theme: {
     extend: {
-      backdropFilter: {
-        none: "none",
-        blur: "blur(20px)",
+      boxShadow: {
+        input: `0px 2px 3px -1px rgba(0,0,0,0.1), 0px 1px 0px 0px rgba(25,28,33,0.02), 0px 0px 0px 1px rgba(25,28,33,0.08)`,
       },
     },
-    variants: {
-          extend: {
-            backdropFilter: ['responsive'],
-          },
+  },
+  plugins: [
+    addVariablesForColors,
+    function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          "bg-dot-thick": (value) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="2.5"></circle></svg>`
+            )}")`,
+          }),
         },
-        plugins: [
-          require('tailwindcss-filters'),
-          addVariablesForColors
-       ],
-},
-  plugins: [addVariablesForColors],
-});
+        { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
+      );
+    },
+  ],
+};
 
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
 function addVariablesForColors({
   addBase,
   theme
