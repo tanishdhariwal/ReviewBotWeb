7de706e65a3bb348002641fa4134b3003b5a972a
WaryFriend456 <kushalbang456@gmail.com>
2024-12-17 00:21:51 +0530

    fix: uncomment JWT token generation in login function

--- backend/Controllers/userControllers.js
+++ backend/Controllers/userControllers.js
@@ -67,13 +67,13 @@ const login = async (req, res) => {
 
     // just chill, we can use it if we get any serious issues
 
-  //   const token = jwt.sign(
-  //     { id: gotuser._id, username: gotuser.username },
-  //     process.env.JWT_SECRET,
-  //     {
-  //       expiresIn: "1h",
-  //     }
-  //   );
+    const token = jwt.sign(
+      { id: gotuser._id, username: gotuser.username },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1h",
+      }
+    );
 
     if (req.headers["device-type"] === "Mobile") {
       console.log("Mobile device detected");
